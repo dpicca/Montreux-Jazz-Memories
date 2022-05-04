0fff23ee9dfc42fc2253c66af6a0ab45d4bdcff9
johancuda <61778212+johancuda@users.noreply.github.com>
2022-05-04 18:13:10 +0200

    Update main.py

--- testGoogleCLoud/main.py
+++ testGoogleCLoud/main.py
@@ -1,6 +1,21 @@
 # Imports the Google Cloud client library
 from google.cloud import speech
 import io
+<<<<<<< Updated upstream
+=======
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+
+def chunked():
+    sound = AudioSegment.from_wav('MJ_test.wav')
+    audio_chunks = split_on_silence(sound, min_silence_len=500, silence_thresh=-40 )
+
+    for i, chunk in enumerate(audio_chunks):
+        output_file = "/Users/johancuda/PycharmProjects/pythonProject/testGoogleCLoud/chunks/chunk{0}.wav".format(i)
+        print("Exporting file", output_file)
+        chunk.export(output_file, format="wav")
+>>>>>>> Stashed changes
 
 
 def main():
@@ -14,6 +29,7 @@ def main():
     with io.open(file_name, 'rb') as f:
         audio_file=f.read()
 
+<<<<<<< Updated upstream
     #audio = speech.RecognitionAudio(content=audio_file)
     audio = speech.RecognitionAudio(uri="gs://montreux_test/355009.wav")
 
@@ -21,13 +37,24 @@ def main():
         language_code="fr-FR",
         audio_channel_count=1,
         enable_automatic_punctuation=True,
+=======
+    audio = speech.RecognitionAudio(content=audio_file)
+
+    config = speech.RecognitionConfig(
+        language_code="en-US",
+        audio_channel_count=1,
+>>>>>>> Stashed changes
     )
     operation = client.long_running_recognize(config=config, audio=audio)
 
     print("Waiting for operation to complete...")
+<<<<<<< Updated upstream
     response = operation.result()
 
     #status update every 3 minutes for
+=======
+    response = operation.result(timeout=90)
+>>>>>>> Stashed changes
 
     for result in response.results:
         # The first alternative is the most likely one for this portion.
